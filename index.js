b79ff03c02dd03171e71cd7eb11339398a69f0e4
Elhamirzaheidar <emirzaheidar@gmail.com>
2024-04-07 01:23:05 -0700

    ChangeCity

---
+++ index.js
@@ -0,0 +1,5 @@
+function Handlerfunction() {
+  let firstInputs = document.querySelector(".firstInput").value;
+  let CityName = document.querySelector(".CityName");
+  CityName.innerHTML = firstInputs;
+}
